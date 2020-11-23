0b161b438833f2a1e00940d2c49a5315b44ff798
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2020-11-23 08:26:05 +0000

    created Moveable with standard and capture move methods

---
+++ lib/modules/moveable.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Moveable
+  def execute_standard_move(start, target)
+    moving_piece = start.reset
+
+    target.occupying_piece = moving_piece
+    target.is_occupied = true
+
+    moving_piece.moves_made += 1
+  end
+
+  def execute_capture_move(start, target, chess_board)
+    moving_piece = start.reset
+    captured_piece = target.reset
+
+    target.occupying_piece = moving_piece
+    target.is_occupied = true
+
+    if captured_piece.color == 'white'
+      chess_board.white_graveyard.push(captured_piece)
+    else
+      chess_board.black_graveyard.push(captured_piece)
+    end
+
+    moving_piece.moves_made += 1
+  end
+end
